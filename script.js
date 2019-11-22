671020c1a2c730265cf4bdecff32da6847b3f512
Tatiana <tatianaw83@gmail.com>
2019-11-21 19:51:24 -0800

    Added some functionality.

--- script.js
+++ script.js
@@ -5,214 +5,203 @@ Need to set up turns so next we can switch until pokemon is dead.
 Upon picking, each pokemon needs to show up, buttons need to be deleted
 then buttons for moves need to be added*/
 
-
-
-
-
-
 //setting up Type objects
 function Type(name, weakness, strength){
-    this.name = name;
-    this.weakness = weakness;
-    this.strength = strength;
-  };
-  
-  //setting up types
-  let fireType = new Type("Fire", "water");
-  let waterType = new Type("water", "grass");
-  let grassType = new Type("grass", "fire");
-  
-  fireType.weakness = waterType;
-  waterType.weakness = grassType;
-  grassType.weakness = fireType;
-  
-  //establishing move object
-  function Move(name, type, strength){
-    this.name = name;
-    this.type = type;
-    this.strength = strength;
-  };
-  
-  //setting up moves
-  let Tackle = new Move("Tackle", "Normal", 2);
-  let Ember = new Move("Ember", fireType, 3);
-  let Scratch = new Move("Scratch", "Normal", 2);
-  let vineWhip = new Move("Vine Whip", grassType, 3);
-  
-  //setting up pokemon object
-  function Pokemon(name, type, move, HP){
-    this.name = name;
-    this.type = type;
-    this.move = move;
-    this.HP = HP;
-  };
-  
-  //creating the pokemon 
-  let Charmander = new Pokemon("Charmander", fireType, [Tackle, Ember], 20);
-  let Bulbasaur = new Pokemon("Bulbasaur", grassType, [Scratch, vineWhip], 20);
-  let Squirtle = new Pokemon("Squirtle", waterType, [Tackle], 20);
- 
-  const charImg = document.createElement("IMG");
-  charImg.alt = "Picture of Charmander";
-  charImg.setAttribute('class', 'photo');
-  charImg.src="sprites/charmander-active.png";
-  charImg.style.width = "200px";
-  charImg.style.height = "200px"
- 
-  const bulbaImg = document.createElement("IMG");
-  bulbaImg.alt = "Picture of Bulbasaur";
-  bulbaImg.setAttribute('class', 'photo');
-  bulbaImg.src="sprites/bulbasaurr-static-2.png";
-  bulbaImg.style.width = "200px";
-  bulbaImg.style.height = "200px"
- 
-  const squirtImg = document.createElement("IMG");
-  squirtImg.alt = "Picture of Squirtle";
-  squirtImg.setAttribute('class', 'photo');
-  squirtImg.src="sprites/squirtle-active-2.png";
-  squirtImg.style.width = "200px";
-  squirtImg.style.height = "200px"
-  
-
-  
-  function player1turn(){
-    if(turn === 0){
-      turn += 1;
-      console.log(turn);
-      //checkForWinner();
-    } else if (turn === 1){
-      console.log("player 2 turn");
-      
-  }
-  }
-    
-    function player2turn(){
-    if(turn === 1){
-      turn -= 1;
-      alert("player 1 next");
-      //checkForWinner();
-    } else if(turn === 0){
-              alert("player 1 turn")
-      }
-  }
-  
-  /* function checkForWinner(){
-    if (player1.hp === 0){
-      alert("player2 wins!")
-      turn = 2;
-      $("#player2btn").show();
-      $("#player1btn").show();
-    } else if (player2.hp === 0){
-      alert("player1 wins!");
-      turn = 2;
-      $("#player2btn").show();
-      $("#player1btn").show();
-         
-    }
-  }
-*/
-
-let player1 = {
-  pokemonChoice : undefined
-
+  this.name = name;
+  this.weakness = weakness;
+  this.strength = strength;
 };
-let player2 = {
-  pokemonChoice : undefined
-  
+
+//setting up types
+let fireType = new Type("Fire");
+let waterType = new Type("water");
+let grassType = new Type("grass");
+
+fireType.weakness = waterType;
+waterType.weakness = grassType;
+grassType.weakness = fireType;
+fireType.strength = grassType;
+waterType.strength = waterType;
+grassType.strength = waterType;
+
+//establishing move object
+function Move(name, type, power){
+  this.name = name;
+  this.type = type;
+  this.power = power
 };
+
+//setting up moves
+let Tackle = new Move("Tackle", "Normal", 2);
+let Ember = new Move("Ember", fireType, 3);
+let Scratch = new Move("Scratch", "Normal", 2);
+let vineWhip = new Move("Vine Whip", grassType, 3);
+let waterGun = new Move("Water Gun", waterType, 3)
+
+//setting up pokemon object
+function Pokemon(name, Type, Move, HP){
+  this.name = name;
+  this.Type = Type;
+  this.Move = Move;
+  this.HP = HP;
+};
+
+//creating the pokemon 
+let Charmander = new Pokemon("Charmander", fireType, [Tackle, Ember], 20);
+let Bulbasaur = new Pokemon("Bulbasaur", grassType, [Scratch, vineWhip], 20);
+let Squirtle = new Pokemon("Squirtle", waterType, [Tackle, waterGun], 20);
+
+const charImg = document.createElement("IMG");
+charImg.alt = "Picture of Charmander";
+charImg.setAttribute('class', 'photo');
+charImg.src= "sprites/charmander-active.png";
+charImg.style.width = "200px";
+charImg.style.height = "200px";
+
+const bulbaImg = document.createElement("IMG");
+bulbaImg.alt = "Picture of Bulbasaur";
+bulbaImg.setAttribute('class', 'photo');
+bulbaImg.src= "sprites/bulbasaur-active.gif";
+bulbaImg.style.width = "200px";
+bulbaImg.style.height = "200px";
+
+const squirtImg = document.createElement("IMG");
+squirtImg.alt = "Picture of Squirtle";
+squirtImg.setAttribute('class', 'photo');
+squirtImg.src= null;
+squirtImg.style.width = "200px";
+squirtImg.style.height = "200px";
+
+
+
 
 let turn = 0;
+
+function checkTurn(){
+if (turn === 0){
+  turn += 1;
+} else if (turn === 1) {
+  turn -= 1
+
+}
+}
+
+let player1 = {
+pokemonChoice : undefined
+};
+let player2 = {
+pokemonChoice : undefined
+};
+
 let p1selected = false;
 let p2selected = false;
 let battleSequence = false;
 
-function checkPokemon(){
-  if ((player1.pokemonChoice != undefined) && (player2.pokemonChoice != undefined)){
-    console.log("pokemon Selected")
-    $(".pokeSelectButtons").hide();
-    battleSequence = true;
-  }
-}
 
+function checkPokemon(){
+if ((player1.pokemonChoice != undefined) && (player2.pokemonChoice != undefined)){
+  console.log("pokemon Selected")
+  $(".pokeSelectButtons").hide();
+  $("#queryDisplay").hide();
+  battleSequence = true;
+  moveSetup();
+}
+};
 
 $("#charSelect").click(function(){
-  if (turn === 0){
-    player1.pokemonChoice = Charmander;
-    $("#player1").html(charImg);
-    p1selected === true;
-    turn = 1;
-    checkPokemon()
-    ;
-  } else if (turn === 1){
-    player2.pokemonChoice = Charmander;
-    $("#player2").html(charImg);
-    p2selected === true;
-    turn = 0;
-    checkPokemon()
-  }
-
-})
-
-  $("#bulbaSelect").click(function(){
-  
-    if (turn === 0){
-      player1.pokemonChoice = Bulbasaur;
-      $("#player1").html(bulbaImg);
-      p1selected = true;
-      turn = 1;
-      checkPokemon()
-    } else if (turn === 1){
-      player2.pokemonChoice = Bulbasaur;
-      $("#player2").html(bulbaImg);
-      p2selected = true;
-      turn = 0;
-      checkPokemon()
-    }
-
-  })
-
-  $("#squirtSelect").click(function(){
-     if (turn = 0){
-      player1.pokemonChoice = Squirtle;
-      $("#player1").html(squirtImg);
-      p1selected = true;
-      turn = 1;
-      checkPokemon();
-    } else if (turn = 1){
-      player2.pokemonChoice = Squirtle
-      $("#player2").html(squirtImg);
-      p2selected = true;
-      turn = 0;
-      checkPokemon();
-    }
-  })
-
+if (turn === 0){
  
-
-
+  player1.pokemonChoice = Charmander;
+  $("#player1").html(charImg);
+  p1selected === true;
+   checkTurn();
+} else {
   
-  // create two buttons for each pokemon 
-  if (battleSequence == true){
-    createMoveButtons();
-  }
+  player2.pokemonChoice = Charmander;
+  $("#player2").html(charImg);
+  p2selected === true;
+  checkPokemon()
+  checkTurn();
+ 
+}
 
-  // attack sequence
-  function runAttack(){
+});
 
-    //need to make the move dynamic based on what user chooses
-    if(Charmander.move[1].type == Bulbasaur.type.weakness){
-      console.log("works!")
-      //
-    } else {
-      console.log("error")
-    }
+$("#bulbaSelect").click(function(){
+
+  if (turn === 0){
+    player1.pokemonChoice = Bulbasaur;
+    $("#player1").html(bulbaImg);
+    p1selected = true;
+    checkTurn();
+  }else {
+    
+    player2.pokemonChoice = Bulbasaur;
+    $("#player2").html(bulbaImg);
+    p2selected = true;
+    checkPokemon();
+    checkTurn();
     
   }
-  
 
+});
 
-function createMoveButtons(){
-  for (i = 0; i < player1.pokemonChoice.move.length; i++){
-    $("button").appendTo("#player1");
+$("#squirtSelect").click(function(){
+   if (turn === 0){
+    player1.pokemonChoice = Squirtle;
+    $("#player1").html(squirtImg);
+    p1selected = true;
+    checkTurn();
+  } else {
+    player2.pokemonChoice = Squirtle
+    $("#player2").html(squirtImg);
+    p2selected = true;
+    checkPokemon();
+    checkTurn();
   }
+});
+
+function moveSetup(){
+if (battlesequence = true){
+  for (i = 0; i < player1.pokemonChoice.Move.length; i++)
+  $("<button>" + player1.pokemonChoice.Move[i].name + "</button>").addClass("moveButtons").attr('id', player1.pokemonChoice.Move[i].name).appendTo("#player1");
+  
+  ;
+  
+  for (i = 0; i < player2.pokemonChoice.Move.length; i++)
+  $("<button>" + player2.pokemonChoice.Move[i].name + "</button>").addClass("moveButtons").attr('id', player2.pokemonChoice.Move[i].name).appendTo("#player2");
 }
+};
+
+let attacks = {
+p1Attack: undefined,
+p2Attack: undefined
+}
+
+function runAttacks(){
+console.log(p1Attack);
+console.log(p2Attack);
+}
+
+// make this some kind of loop 
+
+$("#player1").on("click", "#Tackle", function() {
+p1Attack = player1.pokemonChoice.Move[0];
+
+});
+
+$("#player1").on("click", "#Ember", function() {
+p1Attack = player1.pokemonChoice.Move[1];
+runAttacks();
+});
+
+$("#player2").on("click", "#Scratch", function() {
+p2Attack = player2.pokemonChoice.Move[0];
+runAttacks();
+});
+
+$("#player2").on("click", "#Vine Whip", function() {
+p2Attack = player2.pokemonChoice.Move[1];
+runAttacks();
+});
+
