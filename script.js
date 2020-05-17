0e4087a092334c576f8436c19cc4cca921b39c44
Tatiana <tatianaw83@gmail.com>
2020-05-16 20:11:51 -0700

    Changed objects to classes

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 
 
-//setting up Type objects
-function Type(name, weakness, strength){
-  this.name = name;
-  this.weakness = weakness;
-  this.strength = strength;
-};
+class Types {
+  constructor(name, strength, weakness){
+    this.name = name;
+    this.weakness = weakness;
+    this.strength = strength;
+  }
+}
 
-//setting up types
-let fireType = new Type("Fire");
-let waterType = new Type("water");
-let grassType = new Type("grass");
+const fireType = new Types("Fire", "Grass", "Water")
+const waterType = new Types("Water", "Fire", "Grass")
+const grassType = new Types("Grass", "Water",  "Fire")
+const normalType = new Types("Normal")
 
-fireType.weakness = waterType;
-waterType.weakness = grassType;
-grassType.weakness = fireType;
-fireType.strength = grassType;
-waterType.strength = waterType;
-grassType.strength = waterType;
 
 //establishing move object
-function Move(name, type, power){
-  this.name = name;
-  this.type = type;
-  this.power = power
-};
-
+class Move{
+  constructor(name, type, power){
+    this.name = name;
+    this.type = type;
+    this.power = power
+  }
+}
+  
 //setting up moves
-let Tackle = new Move("Tackle", "Normal", 2);
-let Ember = new Move("Ember", fireType, 3);
-let Scratch = new Move("Scratch", "Normal", 2);
-let vineWhip = new Move("Vine Whip", grassType, 3);
-let waterGun = new Move("Water Gun", waterType, 3)
+const Tackle = new Move("Tackle", normalType, 2);
+const Ember = new Move("Ember", fireType, 3);
+const Scratch = new Move("Scratch", normalType, 2);
+const vineWhip = new Move("Vine Whip", grassType, 3);
+const waterGun = new Move("Water Gun", waterType, 3)
 
-//setting up pokemon object
-function Pokemon(name, Type, Move, HP){
-  this.name = name;
-  this.Type = Type;
-  this.Move = Move;
-  this.HP = HP;
-};
+
+class Pokemon{
+  constructor(name, Type, Move, HP){
+    this.name = name;
+    this.Type = Type;
+    this.Move = Move;
+    this.HP = HP;
+  }
+
+}
 
 //creating the pokemon 
 let Charmander = new Pokemon("Charmander", fireType, [Tackle, Ember], 20);
@@ -76,12 +76,11 @@ const bulbasaurImg = {
 let turn = 0;
 
 function checkTurn(){
-if (turn === 0){
-  turn += 1;
-} else if (turn === 1) {
-  turn -= 1
-
-}
+  if (turn === 0){
+    turn += 1;
+  } else if (turn === 1) {
+    turn -= 1
+  }
 }
 
 let player1 = {
@@ -91,9 +90,6 @@ let player2 = {
 pokemonChoice : undefined,
 };
 
-let p1selected = false;
-let p2selected = false;
-let battleSequence = false;
 
 
 
